701dc5d7ee4aa13291829f38175dcc13ae46af23
Lucas Gardini <lucasgardini.dias@hotmail.com>
2021-07-20 21:51:13 -0300

    Initial Commit

---
+++ code/index.js
@@ -0,0 +1,22 @@
+const main = document.getElementById("main");
+const login = document.getElementById("user");
+const password = document.getElementById("password");
+const button = document.getElementById("loginBtn");
+
+function validateForm() {
+	if (login.value.length > 8 && password.value.length > 8) {
+		button.className = "active";
+	} else {
+		button.className = "";
+	}
+}
+
+window.onload = () => {
+	window.addEventListener("resize", () => {
+		if (window.innerWidth < 710) {
+			main.className = "small";
+		} else {
+			main.className = "";
+		}
+	});
+};
